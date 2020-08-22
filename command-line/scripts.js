c4e03977ed329da895c3ef8e9b60ca7cfbbeac8f
Josue Majano <josuemajano@josues-mbp.attlocal.net>
2020-08-21 20:47:44 -0500

    Initial commit

---
+++ command-line/scripts.js
@@ -0,0 +1,8 @@
+// This is a JavaScript file
+
+var myFirstName = "Josue" // this is a string
+
+let myArray = [1, 2, 3, "Some string "]; // this is a array
+
+let highScore = 10; //Integer
+var iWin = 2000;
